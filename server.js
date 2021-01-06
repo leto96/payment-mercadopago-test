5a0faaef0cbe115c560803f2957f84566933f0c9
matheusleto08 <matheusleto08@gmail.com>
2021-01-06 07:25:35 -0300

    Revert "Configs for uploading in Heroku"
    This reverts commit de15a7c1e5081f6318b6e7cbc32772ea97acda39.

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const HARDCODEDEV = false;
+const HARDCODEDEV = true;
 (process.env.NODE_ENV === 'development' || HARDCODEDEV) ? require('dotenv').config(): null;
 console.log( process.env.NODE_ENV === 'development' || HARDCODEDEV ? 'development' : 'production' );
 
@@ -50,15 +50,5 @@ app.post('/api/payment', async (req, res) => {
     res.send({status: 'Error', message: 'Something went wrong'});
   }
 });
-
-if(process.env.NODE_ENV === 'production'){
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, 'client/build')));
-    
-  // Handle React routing, return all requests to React app
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-}
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
