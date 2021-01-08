1cfdaf0e76da4249e85877c6d22aa652487e5481
matheusleto08 <matheusleto08@gmail.com>
2021-01-07 21:37:40 -0300

    Removed test routes

--- server.js
+++ server.js
@@ -15,16 +15,6 @@ const privateToken = process.env.MERCADO_PAGO_ACCESS_TOKEN;
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/api/hello', (req, res) => {
-  res.send({ express: 'Hello From Express' });
-});
-
-app.post('/api/world', (req, res) => {
-  res.send(
-    `I received your POST request. This is what you sent me: ${req.body.post}`,
-  );
-});
-
 app.post('/api/payment', async (req, res) => {
   // Create the test user
   try {
